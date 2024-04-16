c98f0551858a4a31d2d28f9831ac868b92669f24
Saususge <135770069+Saususge@users.noreply.github.com>
2024-04-16 17:29:23 +0900

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 
 typedef struct BitmapFileHeader
 {
-	unsigned short bfType;           // BMP magic num
+    unsigned short bfType;           // BMP magic num
     unsigned int   bfSize;           // file size
     unsigned short bfReserved1;      // reserved (should be zero)
     unsigned short bfReserved2;      
@@ -21,7 +21,7 @@ typedef struct BitmapFileHeader
 
 typedef struct BitmapInfoHeader
 {
-	unsigned int   biSize;           // size of current struct
+    unsigned int   biSize;           // size of current struct
     int            biWidth;          // width of img
     int            biHeight;         // height of img
     unsigned short biPlanes;         // color plane nums
@@ -36,7 +36,7 @@ typedef struct BitmapInfoHeader
 
 typedef struct RGBTriple
 {
-	unsigned char rgbtBlue;          // B
+    unsigned char rgbtBlue;          // B
     unsigned char rgbtGreen;         // G
     unsigned char rgbtRed;           // R
 } RGBTriple;
@@ -45,62 +45,61 @@ typedef struct RGBTriple
 
 int main(void)
 {
-	srand(time(NULL)); // setting rand seed to time
+    srand(time(NULL)); // setting rand seed to time
 
-	// moyai img file random namer
-	int r_num = rand() % 144 + 1;
-	char directory[30] = "moyai/";
-	char file_num [5];
-	sprintf(file_num, "%d", r_num);
-	strcat(directory, file_num);
-	strcat(directory, ".bmp");
+    // moyai img file random namer
+    int r_num = rand() % 144 + 1;
+    char directory[30] = "moyai/";
+    char file_num [5];
+    sprintf(file_num, "%d", r_num);
+    strcat(directory, file_num);
+    strcat(directory, ".bmp");
 	
-	FILE* fpBmp;
-	FILE* fpTxt;
-	BitmapFileHeader fileHeader;
-	BitmapInfoHeader infoHeader;
+    FILE* fpBmp;
+    FILE* fpTxt;
+    BitmapFileHeader fileHeader;
+    BitmapInfoHeader infoHeader;
 
-	unsigned char* image;
-	int size;
-	int width, height;
-	int padding;
+    unsigned char* image;
+    int size;
+    int width, height;
+    int padding;
 	
-	char ascii[] = { '#', '#', '@', '%', '=', '+', '*', '^', ':' , ';', '-', '`', '"', '.', ' ' };    // letters for drawing
+    char ascii[] = { '#', '#', '@', '%', '=', '+', '*', '^', ':' , ';', '-', '`', '"', '.', ' ' };    // letters for drawing
 
-	fpBmp = fopen(directory, "rb");
+    fpBmp = fopen(directory, "rb");
 
     fread(&fileHeader, sizeof(BitmapFileHeader), 1, fpBmp);
     fread(&infoHeader, sizeof(BitmapInfoHeader), 1, fpBmp);
 
-	size = infoHeader.biSizeImage;    // pixel data size
+    size = infoHeader.biSizeImage;    // pixel data size
     width = infoHeader.biWidth;       // width size of img
     height = infoHeader.biHeight;     // height size of img
 
-	// calculating padding size
-	padding = (PIXEL_ALIGN - ((width * PIXEL_SIZE) % PIXEL_ALIGN)) % PIXEL_ALIGN;
+    // calculating padding size
+    padding = (PIXEL_ALIGN - ((width * PIXEL_SIZE) % PIXEL_ALIGN)) % PIXEL_ALIGN;
 
-	if (size == 0)    // getting padding size when pixel size is 0
-    {
-        
-        size = (width * PIXEL_SIZE + padding) * height;
-    }
+    if (size == 0)    // getting padding size when pixel size is 0
+        {
+            size = (width * PIXEL_SIZE + padding) * height;
+        }
 
-	image = malloc(size);  
+    image = malloc(size);  
 
-	// move file pointer to starting point of pixel data
+    // move file pointer to starting point of pixel data
     fseek(fpBmp, fileHeader.bfOffBits, SEEK_SET);
 
-	if (fread(image, size, 1, fpBmp) < 1)
-    {
-        fclose(fpBmp);
-        return 0;
-    }
+    if (fread(image, size, 1, fpBmp) < 1)
+	{
+            fclose(fpBmp);
+            return 0;
+	}
 
-	fclose(fpBmp);
+    fclose(fpBmp);
 
-	fpTxt = fopen("ascii.txt", "w"); 
+    fpTxt = fopen("ascii.txt", "w"); 
 
-	// loop y * x times (height * width)
+    // loop y * x times (height * width)
     for (int y = height - 1; y >= 0; y--)
     {
         for (int x = 0; x < width; x++)
@@ -108,7 +107,7 @@ int main(void)
            
             int index = (x * PIXEL_SIZE) + (y * (width * PIXEL_SIZE)) + (padding * y);
 
-			// convert current pixel address to RGBtriple pointer and save it
+	    // convert current pixel address to RGBtriple pointer and save it
             RGBTriple *pixel = (RGBTriple *)&image[index];
 
             // get color from rgb struct
@@ -123,24 +122,24 @@ int main(void)
             char c = ascii[gray * sizeof(ascii) / 256];
 
             // save twice
-			// because ascii art is too weird when it saves only once
+	    // because ascii art is too weird when it saves only once
             fprintf(fpTxt, "%c%c", c, c);
         }
 
         fprintf(fpTxt, "\n");
     }
 
-	fclose(fpTxt);
-	char c;
-	fpTxt = fopen("ascii.txt", "r");
+    fclose(fpTxt);
+    char c;
+    fpTxt = fopen("ascii.txt", "r");
 	
-	// print in console
-	while ((c = fgetc(fpTxt)) != EOF) {
-    	printf("%c", c);
+    // print in console
+    while ((c = fgetc(fpTxt)) != EOF) {
+    printf("%c", c);
 }
-	fclose(fpTxt);
+    fclose(fpTxt);
 
-	free(image);
+    free(image);
 
-	return 0;
+    return 0;
 }
